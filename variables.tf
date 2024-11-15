78c62657f36d5b072e1bdaf39edfbae8d7a779bb
Sujin Kim <su10jin11@khu.ac.kr>
2024-11-15 17:09:45 +0900

    feat : 변수 수정

--- variables.tf
+++ variables.tf
@@ -3,10 +3,7 @@ variable "aws_region" {
   default     = "ap-northeast-2"
 }
 
-variable "pem_key_path" {
-  description = "Path to the public key file for SSH access"
-  default  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDLARx0Ijk8NAyak4Yuihh3B3V8wtGLLqO/GjllT+xiiL9sU2vFoeuBD+PrZtzmSaQHgisCvOV4agpJ8rp1GloGjkitQLScGWpHdgz9/cKBv+2D8OZI0hiXMDh6ntzkLeJ9Z59AYq2XgmB7dKaHIpy/2gzg5bo75T6EzsFY85Rru2uCFXpFHNMqiOBygCDBI2lxQ/qircx6CU6gvG2NtckjxaePD2GWTrISqyWKQc/U2WPJVh9iNb3jg1z1f8g2YQNdVcsOImqvYlTzI8WnOAowhanVREiA6gtRtsH5KbPRCJn+C1f9Cw9kURBihfVAxlNO3KMSv826kW83/pt+8/r9 sujin@sujinui-MacBookPro.local"
-}
+
 
 
 variable "instance_type" {
